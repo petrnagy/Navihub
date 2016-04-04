b4a18525ca2a83bbe67768359e8d7c3c894c84fe
Petr Nagy <petr.nagy@outlook.com>
2016-04-04 15:26:12 +0200

    API cache 1h -> 24h

--- app/models/api_cache.rb
+++ app/models/api_cache.rb
@@ -1,15 +1,15 @@
 class ApiCache < ActiveRecord::Base
-  
+
   require 'yaml'
-  
+
   def self.get_from_cache key
     row = self.find_by(key: key)
-    if row && row.updated_at > (Time.now - 3600)
+    if row && row.updated_at > (Time.now - 86400)
       return YAML::load row.data
     end
     return nil
   end
-  
+
   def self.save_to_cache key, data
     data = YAML::dump data
     cache = self.find_by(key: key)
@@ -20,5 +20,5 @@ class ApiCache < ActiveRecord::Base
     end
     cache.save
   end
-  
+
 end
