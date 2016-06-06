722a8ec14864f8fb3b11187a007a9e3498f76bd8
Petr Nagy <petr.nagy@outlook.com>
2016-06-06 12:58:22 +0200

    updated url in NaviBot job

--- app/jobs/NaviBot/src/const.php
+++ app/jobs/NaviBot/src/const.php
@@ -3,5 +3,5 @@
 define('PRODUCTION', true);
 define('VERSION', '0.1.8');
 define('REQUEST_LIMIT', 5);
-define('SERVICE_URL', 'http://beta.navihub.net');
+define('SERVICE_URL', 'https://www.navihub.net');
 define('SEARCH_TPL', '/search/%%term%%/%%order%%-%%dir%%/%%radius%%/%offset%%/@/%%ll%%');
