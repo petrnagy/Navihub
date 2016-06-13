05e9d33fd1b3ad866cb6068cd41cb846e88971d1
Petr Nagy <petr.nagy@outlook.com>
2016-06-13 20:08:20 +0200

    fix: open details with request url instead of actual one

--- app/assets/javascripts/Detail.js
+++ app/assets/javascripts/Detail.js
@@ -218,8 +218,8 @@ Detail.prototype = {
         */
         buildDetailUrl: function(data, that) {
             that = that || this;
-            var loc = that.di.locator.getLocation();
-            var ll = loc.lat. toFixed(7).toString() + ',' + loc.lng. toFixed(7).toString();
+            var loc = that.di.locator.getRequestLocation() || that.di.locator.getLocation();
+            var ll = loc.lat.toFixed(7).toString() + ',' + loc.lng.toFixed(7).toString();
             var name = encodeURIComponent(data.ascii_name ? data.ascii_name.toString() : 'show');
             var origin = encodeURIComponent(data.origin.toString());
             var id = encodeURIComponent(data.id.toString());
